83c02a8f07342af249589437790df5fea3a2483c
mirna14 <95093657+mirna14@users.noreply.github.com>
2023-03-06 14:19:48 +0000

    ⭐Adding Api User

--- app.js
+++ app.js
@@ -2,10 +2,14 @@ var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
+
+// Library to log http communication
 var logger = require('morgan');
 
+//importing subroutes
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var apiRouter =  require('./routes/api');
 
 var app = express();
 
@@ -19,8 +23,11 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+
+// Registering routes
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/api', apiRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/api.js
+++ routes/api.js
@@ -3,15 +3,16 @@ var express = require('express');
 //creating a Router Instance 
 var router = express.Router();
 
-// creating the route
+// Creating the route
 router.get('/author', (req, res) =>{
+// Responding to the client using res object
     res.json ({
         "name": "Mirna",
         "lastname": "Diaz",
         "twitter": "@mirna",
         "job": "ITGAM"
     });
-});
+}); // function(req, res){}
 
 //exporting the router
 module.exports = router;
