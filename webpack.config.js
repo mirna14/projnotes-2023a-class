08c6bc723a6da2a4e23da7efe4a1fa82bb7c7ff2
mirna14 <95093657+mirna14@users.noreply.github.com>
2023-04-24 02:48:14 +0000

    💢Renombrando a index del cliente

--- webpack.config.js
+++ webpack.config.js
@@ -22,7 +22,9 @@ module.exports = {
     // of the project's static files
     path: path.resolve(__dirname, "public"),
     // 2.2 Output file name
-    filename: "bundle.js"
+    filename: "bundle.js",
+    //2.3
+    publicPath: '/'
   },
   // Adding a module to webpack
   module: {
