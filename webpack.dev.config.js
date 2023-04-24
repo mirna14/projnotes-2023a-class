916322ae7669bdfa98494d93c39acbdbb26ab95a
mirna14 <95093657+mirna14@users.noreply.github.com>
2023-04-24 03:36:09 +0000

    💫Integración de ESlint con Webpack

--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -6,6 +6,7 @@
 const path = require('path');
 // Importing Extract Plugin
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const ESlintPlugin = require("eslint-webpack-plugin");
 
 // We export a configuration object
 // that will be used by webpack
@@ -66,8 +67,11 @@ module.exports = {
       }
     ]
   },
-  plugins: [new MiniCssExtractPlugin({
+  plugins: [
+    new MiniCssExtractPlugin({
     // Archivo css de salida
     filename: 'styles/app.css'
-  })]
+  }),
+  new ESlintPlugin({})
+]
 }
