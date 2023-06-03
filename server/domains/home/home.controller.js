11b1507044d98d2ccc2f6fe07565ffb1095eecff
mirna14 <95093657+mirna14@users.noreply.github.com>
2023-06-03 04:05:14 +0000

    🚋Rutas de acciones

--- server/domains/home/home.controller.js
+++ server/domains/home/home.controller.js
@@ -6,7 +6,13 @@ const home = (req, res) => {
   const icon = iconSet[Math.floor(Math.random() * 3)];
   res.render('index', { title: 'DWPCII-2023A', icon });
 };
+
+// GET "/index"
+const about = (req, res) => {
+  res.send('⚠️ UNDER CONSTRUCTION: GET /about ⚠️');
+};
 // Controlador Home
 export default {
   home,
+  about,
 };
