0f2825e193b338408791db0530eaa7396c7146f6
mirna14 <95093657+mirna14@users.noreply.github.com>
2023-06-04 07:47:54 +0000

    🚫error conexion

--- server/services/odm.js
+++ server/services/odm.js
@@ -13,15 +13,14 @@ class MongooseOdm {
       // Estableciendo el sistema de promesas
       // por defecto en mongoose
       mongoose.Promise = global.Promise;
-      log.info(`📞📞 Conectando a la base de datos: ${this.url}`);
+      log.info(`📞📞 Conectando a la base de datos ...${this.url}`);
       const connection = await mongoose.connect(this.url);
-      log.info(`📞📞 FINALIZO CONEXION 📞📞 ${typeof connection}`);
       return connection;
     } catch (error) {
-      log.error('OCURRIO ERROR');
-      throw new Error(
+      log.error(
         `🥀 No se pudo establecer conexión a la base de datos debido a: ${error.message} 🥀`
       );
+      return null;
     }
   }
 }
