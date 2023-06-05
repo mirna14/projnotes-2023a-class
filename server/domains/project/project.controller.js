659153bcaf6f444ad2d4b777c9ec1902b7734364
mirna14 <95093657+mirna14@users.noreply.github.com>
2023-06-05 06:23:40 +0000

    👌Validacion en vustas

--- server/domains/project/project.controller.js
+++ server/domains/project/project.controller.js
@@ -1,13 +1,17 @@
+// Importing winston logger
 import log from '../../config/winston';
+
 // Actions methods
 // GET "/project"
 const showDashboard = (req, res) => {
   res.send('⚠️ UNDER CONSTRUCTION: GET /project ⚠️');
 };
+
 // GET "/project/add"
 const add = (req, res) => {
   res.render('project/addView');
 };
+
 // POST "/project/add"
 const addPost = (req, res) => {
   // Rescatando la info del formulario
@@ -16,25 +20,28 @@ const addPost = (req, res) => {
   // se le informa al cliente
   if (validationError) {
     log.info('Se entrega al cliente error de validación de add Project');
-    res.status(422).json(validationError);
-  } else {
-    // En caso de que pase la validación
-    // Se desestructura la información
-    // de la peticion
-    const { validData: project } = req;
-    // Se contesta la información
-    // del proyecto al cliente
-    res.status(200).json(project);
+    // Se desestructuran los datos de validación
+    const { value: project } = validationError;
+    // Se extraen los campos que fallaron en la validación
+    const errorModel = validationError.inner.reduce((prev, curr) => {
+      // Creando una variable temporal para
+      // evitar el error "no-param-reassing"
+      const workingPrev = prev;
+      workingPrev[`${curr.path}`] = curr.message;
+      return workingPrev;
+    }, {});
+    return res.status(422).render('project/addView', { project, errorModel });
   }
-  // Extrayendo la informacion
-  // del formulario
-  const { name, description } = req.body;
-  // Regresando al cliente la información recabada
-  res.status(200).json({
-    name,
-    description,
-  });
+  // En caso de que pase la validación
+  // Se desestructura la información
+  // de la peticion
+  const { validData: project } = req;
+  // Se contesta la información
+  // del proyecto al cliente
+  log.info('Se entrega al cliente información del proyecto cargado');
+  return res.status(200).json(project);
 };
+
 // Controlador user
 export default {
   // Action Methods
