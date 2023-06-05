896b092b78bec6dc86765a2050866ddfbca88f46
mirna14 <95093657+mirna14@users.noreply.github.com>
2023-06-05 04:19:59 +0000

    🧪Procesando Formulario

--- server/domains/project/project.controller.js
+++ server/domains/project/project.controller.js
@@ -7,9 +7,21 @@ const showDashboard = (req, res) => {
 const add = (req, res) => {
   res.render('project/addView');
 };
+// POST "/project/add"
+const addPost = (req, res) => {
+  // Extrayendo la informacion
+  // del formulario
+  const { name, description } = req.body;
+  // Regresando al cliente la información recabada
+  res.status(200).json({
+    name,
+    description,
+  });
+};
 // Controlador user
 export default {
   // Action Methods
   showDashboard,
   add,
+  addPost,
 };
