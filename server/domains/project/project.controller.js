c8bb810bf6ca7817c7d59d632e591cb1ff642095
mirna14 <95093657+mirna14@users.noreply.github.com>
2023-06-10 22:07:28 +0000

    📀Persistencia de datos

--- server/domains/project/project.controller.js
+++ server/domains/project/project.controller.js
@@ -1,19 +1,19 @@
 // Importing winston logger
 import log from '../../config/winston';
-
+// Importando el modelo
+import ProjectModel from './project.model';
+// Importando Httperrors
 // Actions methods
 // GET "/project"
 const showDashboard = (req, res) => {
   res.send('⚠️ UNDER CONSTRUCTION: GET /project ⚠️');
 };
-
 // GET "/project/add"
 const add = (req, res) => {
   res.render('project/addView');
 };
-
 // POST "/project/add"
-const addPost = (req, res) => {
+const addPost = async (req, res) => {
   // Rescatando la info del formulario
   const { errorData: validationError } = req;
   // En caso de haber error
@@ -36,12 +36,20 @@ const addPost = (req, res) => {
   // Se desestructura la información
   // de la peticion
   const { validData: project } = req;
-  // Se contesta la información
-  // del proyecto al cliente
-  log.info('Se entrega al cliente información del proyecto cargado');
-  return res.status(200).json(project);
+  try {
+    // Creando la instancia de un documento
+    // con los valores de 'project'
+    const savedProject = await ProjectModel.create(project);
+    // Se contesta la información del proyecto al cliente
+    log.info('Se entrega al cliente información del proyecto cargado');
+    return res.status(200).json(savedProject);
+  } catch (error) {
+    log.error(
+      'ln 53 project.controller: Error al guardar proyecto en la base de datos'
+    );
+    return res.status(500).json(error);
+  }
 };
-
 // Controlador user
 export default {
   // Action Methods
